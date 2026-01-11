69ae48170aecb665218076e4dd7da5b63449fe0d
Silkmanuel <107256054+Silkmanuel@users.noreply.github.com>
2026-01-11 12:03:58 +0100

    implementing static filtering for rest api

--- demo/src/main/java/com/in28minuts/rest/webservice/demo/user/User.java
+++ demo/src/main/java/com/in28minuts/rest/webservice/demo/user/User.java
@@ -2,12 +2,15 @@ package com.in28minuts.rest.webservice.demo.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
     private Integer id;
     @Size(min=2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
     @Past(message = "Birth date should be in the past")
     private LocalDate birthDate;
